5d5ba58ba3d4443ee0b8f05dd6c68eefb78162d8
Gumiin <jakub.szlachetka@microsoft.wsei.edu.pl>
2022-02-02 11:22:25 +0100

    Initial commit

---
+++ Controllers/DegreeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13316_egzamin_z_asp.net.Models
+{
+    public class DegreeController : Controller
+    {
+        public Dictionary<int, Degree> StudentGrades = new();
+            public int index = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddPage()
+        {
+            return View("Add");
+        }
+        public IActionResult Add(Degree degree)
+        {
+
+            StudentGrades.Add(index, degree);
+            index++;
+            return View("Index", StudentGrades.Values.ToList());
+        }
+        
+    
+    }
+}
---
+++ Models/Degree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13316_egzamin_z_asp.net.Models
+{
+    public class Degree
+    {
+
+        public int Index { get; set; }
+        [Required]
+        public double Grade { get; set; }
+
+        public string Subject { get; set; }
+
+
+    }
+}
